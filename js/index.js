6e3af2d4cb899723aa7f37d039984a1b0afaed44
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-04-22 16:24:29 +0500

    Solution 4 example

---
+++ js/index.js
@@ -0,0 +1,90 @@
+// 1-Misol
+
+// function NumberInArray(numberString) {
+//     const number = parseInt(numberString);
+
+//     if (isNaN(number)) {
+//         return "Invalid number";
+//     }
+
+//     const NumberArray = number.toString().split('').reverse();
+
+//     return NumberArray;
+// }
+
+// const numberString = "34567";
+// console.log(NumberInArray(numberString));
+
+
+
+
+
+
+
+// 2-Misol
+
+// let a = 5;
+// let b = 5;
+
+// if (a == b) {
+//     console.log (true)
+// } else {
+//     console.log(false);
+// }
+
+// Bu yerda oddiy a   b ga tengmi deyilyapti
+
+// if (a === b) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// Bu yerda a va b ni tipigachor tekshirib chiqyapti ya'ni tengligini
+
+
+
+
+
+
+// 3-Misol
+
+// function numberArray(n) {
+//     if (n === 1) {
+//         return [1]
+//     } else {
+//         let arr = numberArray (n - 1)
+//         arr.push(n)
+//         return arr
+//     }
+
+// }
+
+// const Userinput = 10
+// const resultArray = numberArray (Userinput)
+// console.log(resultArray);
+
+
+
+
+
+
+
+// 4-Misol
+
+// function sumArray(arr) {
+//     const arrSum = arr.filter(num => num % 2 !== 0);
+//     const evenSum = arr.filter(num => num % 2 === 0);
+//     return [arrSum, evenSum];
+// }
+  
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const result = sumArray(numbers);
+// console.log(result);
+
+
+
+
+
+
+
